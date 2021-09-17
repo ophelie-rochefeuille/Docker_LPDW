b22d79bd8dcea8563dc2a9847ca910363057b6bf
Pierre <payangar@MC-G-LP-C120-15.local>
2021-09-17 10:51:13 +0200

    database identif

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const Sequelize=require('sequelize');
+
+module.exports=new Sequelize('app_db','user','password',{
+    host:'db',
+    dialect:'mysql',
+    operatorsAliases:false,
+
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    }
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db=require('../config/database')
+db.sync()
+
+db.authenticate()
+    .then(()=>console.log('Database connected...'))
+    .catch(err=>console.log('Error: '+err))
+const {City,Truck,Temperature}=require('./models')
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
