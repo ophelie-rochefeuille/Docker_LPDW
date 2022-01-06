18187e84e2fc341c099be17cec9ddbf87fdde903
Pierre <hervelinp.code@gmail.com>
2022-01-06 17:30:51 +0100

    #12 fix

--- server/src/routes/transactions.api.js
+++ server/src/routes/transactions.api.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../../config/database');
-const { uuidv4 } = require('../core/helpers');
+const { uuid } = require('uuidv4');
 const { Transactions } = require('../models/index');
 
 router.post('/submit', async (req, res) => {
     if (req.body) {
         const transaction = req.body;
-        transaction.id = uuidv4();
+        transaction.id = uuid();
         if (transaction.type === 'start') {
-            transaction.run_id = uuidv4();
+            transaction.run_id = uuid();
         }
         try {
             await Transactions.create(transaction);
