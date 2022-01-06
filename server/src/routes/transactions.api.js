17df509db20623b67feddf729974e15e4a72a5b3
Pierre <hervelinp.code@gmail.com>
2022-01-06 17:27:55 +0100

    #12 transactions create

--- server/src/routes/transactions.api.js
+++ server/src/routes/transactions.api.js
@@ -2,6 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../../config/database');
+const { uuidv4 } = require('../core/helpers');
 const { Transactions } = require('../models/index');
+
+router.post('/submit', async (req, res) => {
+    if (req.body) {
+        const transaction = req.body;
+        transaction.id = uuidv4();
+        if (transaction.type === 'start') {
+            transaction.run_id = uuidv4();
+        }
+        try {
+            await Transactions.create(transaction);
+            res.sendStatus(200);
+        } catch (e) {
+            res.sendStatus(500);
+            console.error(e);
+        }
+    } else {
+        res.sendStatus(404);
+    }
+});
 
 module.exports = router;
